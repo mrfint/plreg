161b75f5e9be22960cfe7b7ab7e38dff6c9061fa
sshkvar <sshkvar@softserveinc.com>
2013-03-27 18:18:20 +0200

    add jackson for support json

---
+++ src/main/java/com/softserve/hms/rest/AppointmentsRestController.java
@@ -0,0 +1,15 @@
+package com.softserve.hms.rest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class AppointmentsRestController {
+
+/*    @RequestMapping(value = "/rest/appointments", method = RequestMethod.GET)
+    @ResponseBody
+    public Appointment getAppointments() {
+
+        return ;
+    }*/
+}
