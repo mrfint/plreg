dbc7befb162bf3fceedb002fe4ecef3e31cf36ea
mshvach <mshvach@softserveinc.com>
2013-03-28 18:34:09 +0200

    correct ddl and test data

--- src/main/resources/com/softserve/hms/db/schema.sql
+++ src/main/resources/com/softserve/hms/db/schema.sql
@@ -1,13 +1,12 @@
 DROP TABLE user IF EXISTS;
 DROP TABLE patient IF EXISTS;
-DROP TABLE user IF EXISTS;
 DROP TABLE appointment IF EXISTS;
 
 CREATE TABLE patient (
   id         INTEGER IDENTITY PRIMARY KEY,
   first_name VARCHAR(30),
   last_name  VARCHAR(30),
-  mid_name VARCHAR(30)
+  mid_name   VARCHAR(30)
 );
 
 CREATE TABLE user (
@@ -15,15 +14,22 @@ CREATE TABLE user (
   first_name VARCHAR(30),
   last_name  VARCHAR(30),
   mid_name VARCHAR(30),
-  specialization VARCHAR(30)
+  login VARCHAR(30),
+  password VARCHAR(30),
 );
 
 CREATE TABLE appointment (
   id         INTEGER IDENTITY PRIMARY KEY,
   patient_id INTEGER,
-  doctor_id  INTEGER,
-  hospital VARCHAR(30)
+  appoint_date DATE,
+  appoint_type VARCHAR(30),
+  location VARCHAR(30),
+  dob DATE,
+  phone VARCHAR(10),
+  mrn VARCHAR(10),
+  pcn VARCHAR(10),
+  resource   VARCHAR(30),
+  hospital   VARCHAR(30)
 );
 
 ALTER TABLE appointment ADD CONSTRAINT fk_app_patient FOREIGN KEY (patient_id) REFERENCES patient (id);
-ALTER TABLE appointment ADD CONSTRAINT fk_app_doctor FOREIGN KEY (doctor_id) REFERENCES user (id);
