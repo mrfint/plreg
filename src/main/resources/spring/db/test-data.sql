26c5f999fd2ee88c7470874fe4dcdec6a5bc1493
mshvach <mshvach@softserveinc.com>
2013-03-28 12:55:34 +0200

    test data

---
+++ src/main/resources/spring/db/test-data.sql
@@ -0,0 +1,20 @@
+INSERT INTO user VALUES (1, 'admin', 'admin','');
+
+INSERT INTO patient VALUES (1, 'James', 'Carter','E');
+INSERT INTO patient VALUES (2, 'Helen', 'Leary','A');
+INSERT INTO patient VALUES (3, 'Linda', 'Douglas','Esd');
+INSERT INTO patient VALUES (4, 'Rafael', 'Ortega','G');
+INSERT INTO patient VALUES (5, 'Henry', 'Stevens','Bd');
+INSERT INTO patient VALUES (6, 'Sharon', 'Jenkins','A');
+
+INSERT INTO doctor VALUES (1, 'Joan', 'Smit','E', 'ophthalmologist');
+INSERT INTO doctor VALUES (2, 'Armin', 'Buren','van','sawbones');
+
+INSERT INTO appointment VALUES (1,1,1,'Hms Test Hospital' );
+INSERT INTO appointment VALUES (2,2,1,'Hms Test Hospital' );
+INSERT INTO appointment VALUES (3,3,1,'Hms Test1 Hospital');
+INSERT INTO appointment VALUES (4,4,2,'Hms Test1 Hospital');
+INSERT INTO appointment VALUES (5,5,2,'Hms Test2 Hospital');
+INSERT INTO appointment VALUES (6,6,2,'Hms Test2 Hospital');
+
+
