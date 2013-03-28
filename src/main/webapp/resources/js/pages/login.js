3b8b29db330155b4daafd35314509b29d7704c9f
unknown <ykozlov@DP151.softservecom.com>
2013-03-28 11:41:14 +0200

    base CSS/JS resource files

---
+++ src/main/webapp/resources/js/pages/login.js
@@ -0,0 +1,10 @@
+/**
+ * User: ykozlov
+ * Date: 3/28/13
+ * Time: 11:30 AM
+ * To change this template use File | Settings | File Templates.
+ */
+(function () {
+    $(document).ready(function () {
+    });
+})();
