f9e68fed1bd08048e7e2cee140715b09992c5e90
aborovyk <aborovyk@homeaway.com>
2013-03-29 16:14:05 +0200

    DAO for paging fixed

--- src/main/java/com/softserve/hms/repository/AppointmentDaoImpl.java
+++ src/main/java/com/softserve/hms/repository/AppointmentDaoImpl.java
@@ -2,12 +2,10 @@ package com.softserve.hms.repository;
 
 import com.softserve.hms.domain.Appointment;
 import com.softserve.hms.domain.Patient;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.simple.ParameterizedBeanPropertyRowMapper;
-import org.springframework.jdbc.core.simple.ParameterizedRowMapper;
-import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import javax.annotation.Resource;
@@ -23,26 +21,21 @@ import java.util.Map;
 public class AppointmentDaoImpl implements AppointmentDao {
 
     private static final String PAGING_QUERY_NO_FILTER
-            = "SELECT LIMIT ?, ? * FROM appointment";
+            = "SELECT LIMIT ?, ? * FROM appointment JOIN patient ON appointment.id = patient.id";
     private static final String PAGING_QUERY_HOSPITAL_FILTER
-            = "SELECT LIMIT ?, ? * FROM appointment WHERE hospital = ?";
+            = "SELECT LIMIT ?, ? * FROM appointment JOIN patient ON appointment.id = patient.id WHERE hospital = ?";
     private static final String PAGING_QUERY_DEPARTMENT_FILTER
-            = "SELECT LIMIT ?, ? * FROM appointment WHERE department = ?";
+            = "SELECT LIMIT ?, ? * FROM appointment JOIN patient ON appointment.id = patient.id WHERE department = ?";
     private static final String PAGING_QUERY_HOSPITAL_DEPARTMENT_FILTER
-            = "SELECT LIMIT ?, ? * FROM appointment WHERE hospital = ? AND department = ?";
+            = "SELECT LIMIT ?, ? * FROM appointment JOIN patient ON appointment.id = patient.id WHERE hospital = ? AND department = ?";
 
     private JdbcTemplate jdbcTemplate;
-    private DataSource dataSource;
-
-//    @Autowired
-//    public AppointmentDaoImpl(JdbcTemplate jdbcTemplate) {
-//        this.jdbcTemplate = jdbcTemplate;
-//    }
+    private SimpleJdbcTemplate simpleJdbcTemplate;
 
     @Resource(name = "dataSource")
     public void setDataSource(DataSource dataSource) {
-        this.dataSource = dataSource;
         this.jdbcTemplate = new JdbcTemplate(dataSource);
+        this.simpleJdbcTemplate = new SimpleJdbcTemplate(dataSource);
     }
 
     @Override
@@ -70,29 +63,45 @@ public class AppointmentDaoImpl implements AppointmentDao {
         return appointments;
     }
 
+    private static final class AppointMantMapper implements RowMapper<Appointment> {
+
+        //map for storing patients to reduce creation of duplicate instances
+        final Map<Integer, Patient> patients = new HashMap<Integer, Patient>();
+
+        @Override
+        public Appointment mapRow(ResultSet resultSet, int i) throws SQLException {
+            Integer patientId = resultSet.getInt("patient.id");
+            Patient patient = patients.get(patientId);
+            if (patient == null) {
+                patient = new Patient();
+                patient.setId(patientId);
+                patient.setFirstName(resultSet.getString("patient.first_name"));
+                patient.setLastName(resultSet.getString("patient.first_name"));
+                patients.put(patientId, patient);
+            }
+            com.softserve.hms.domain.Appointment appointment = new com.softserve.hms.domain.Appointment();
+            appointment.setId(resultSet.getInt("appointment.id"));
+            appointment.setPatient(patient);
+            appointment.setHospital(resultSet.getString("appointment.hospital"));
+            return appointment;
+        }
+    }
+
     @Override
     public List<Appointment> getAppointments(String hospital, String department, long from, long pageSize) {
-        List args = new ArrayList();
-        args.add(from);
-        args.add(pageSize);
-        //define query for filtering
-        String sqlQuery = PAGING_QUERY_NO_FILTER;
         if (hospital != null) {
-            args.add(hospital);
             if (department != null) {
-                args.add(department);
-                sqlQuery = PAGING_QUERY_HOSPITAL_DEPARTMENT_FILTER;
+                return simpleJdbcTemplate.query(PAGING_QUERY_HOSPITAL_DEPARTMENT_FILTER, new AppointMantMapper(),
+                        from, pageSize, hospital, department);
             }
-            else sqlQuery = PAGING_QUERY_HOSPITAL_FILTER;
+            else return simpleJdbcTemplate.query(PAGING_QUERY_HOSPITAL_FILTER, new AppointMantMapper(),
+                    from, pageSize, hospital);
         }
         else {
-            if (department != null) {
-                args.add(department);
-                sqlQuery = PAGING_QUERY_DEPARTMENT_FILTER;
-            }
+            if (department != null)
+                return simpleJdbcTemplate.query(PAGING_QUERY_DEPARTMENT_FILTER, new AppointMantMapper(),
+                        from, pageSize, department);
         }
-        //get appointments.
-        return jdbcTemplate.query(sqlQuery, args.toArray(),
-                ParameterizedBeanPropertyRowMapper.newInstance(Appointment.class));
+        return simpleJdbcTemplate.query(PAGING_QUERY_NO_FILTER, new AppointMantMapper(), from, pageSize);
     }
 }
